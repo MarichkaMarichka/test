54183411602e5978e4761a81ac67d130d31f8c9a
Maria Kulakova <mkulakova17@mail.ua>
2015-09-21 00:06:42 +0300

    demo views for students_list

--- students/views.py
+++ students/views.py
@@ -1,10 +1,24 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Views for Students
 
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': u'Марія',
+         'last_name': u'Кулакова',
+         'ticket': 4,
+         'image': 'img/6mH8tLcqs4s.jpg'},
+        {'id': 2,
+         'first_name': u'Ярина',
+         'last_name': u'Явор',
+         'ticket': 17,
+         'image': 'img/P7giPAciy1c.jpg'},
+)
+    return render(request, 'students/students_list.html',
+    {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = patterns('',
     # Students urls
@@ -26,3 +29,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns+=staticfiles_urlpatterns()
+
+
+
+
+
